db01934211456bf12ce1bf34f5cd784853119260
Ryan Leonard <ryanlfx16@gmail.com>
2025-11-10 22:55:39 +0000

    Changed Function spelling in main file

--- main_files/main.py
+++ main_files/main.py
@@ -1,8 +1,8 @@
 import user_accounts
-from bank_functionality import login, main_menu
+from bank_functionality import login, user_dashboard
 
 # Login and store users data if successful
 user_data = login()
 
 if user_data:
-    main_menu(user_data)
+    user_dashboard(user_data)
