9c06365db3f2e937208454c1b21d55bfe411238c
Ryan Leonard <ryanlfx16@gmail.com>
2025-12-18 16:26:13 +0000

    Cleaned up function notation

--- main_files/main.py
+++ main_files/main.py
@@ -2,12 +2,19 @@ from app_functionality import login, user_dashboard, prompt_login_or_create_acco
 
 
 user_data = None
+
 display_welcome_message()
+
 while True:
     choice = prompt_login_or_create_account()
+    
     if choice == 1:
         user_data = login()
         break
+    
+    # Will create functionality for creating an account
+    elif choice == 2:
+        pass
     
     elif choice not in [1, 2]:
         print("Enter either 1 or 2\n")
